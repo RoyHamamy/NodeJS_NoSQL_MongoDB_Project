a30a553d56e2fb4289b702786ee255f6df797b0e
RoyHamamy <royha@post.bgu.ac.il>
2022-12-31 20:00:19 +0200

    first commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoConnect = require("./util/database").mongoConnect;
+const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
 
@@ -18,21 +18,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('63a9d507c48582331d5f65e2')
+  User.findById("63b063caaaaafe280712ef91")
     .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch((err) => console.log(err));
 });
 
- app.use("/admin", adminRoutes);
- app.use(shopRoutes);
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=> {
-  app.listen(3000);
-});
-
-
+mongoose
+  .connect(
+    "mongodb+srv://Argameshi_12:Argameshi_12@cluster0.qbzzjmt.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Roy",
+          email: "royh69@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
